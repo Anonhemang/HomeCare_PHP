1d5d668d135b6f6736030088cc0ea622e9427a3c
Anonhemang <hemangthakkar2801@gmail.com>
2022-12-30 10:29:39 +0530

    Service Page 1.0

---
+++ Service.php
@@ -0,0 +1,82 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="shortcut icon" type="img/jpg" href="img/logo.jpg">
+    <link rel="stylesheet" href="Service.css">
+    <title>Book Your Service</title>
+</head>
+
+<body>
+
+    <div class="nav">
+        <a href="#Maid" title="Click to view more services"><img src="MaidLogo.png" class="logo" alt=""> Maid</a>
+        <a href="#Security" title="Click to view more services"><img src="SecurityLogo.png" alt="" class="logo"> Security</a>
+        <a href="#Cleaner" title="Click to view more services"><img src="CleanerLogo.png" alt="" class="logo"> Cleaner</a>
+    </div>
+    
+    
+
+    
+    
+    <div class="select_services">
+        <img src="ServicePageLineTop.png" alt="" class="designTop"><br> <h1>Services</h1>
+        <br><img src="ServicePageLineBottom.png" alt="" class="designBottom">
+    </div>
+
+
+
+
+    <div class="maid">
+        <h2>Maid</h2>
+        <img src="HeadLine.png" alt="" class="head_line">
+
+    <div class="maid_img"> 
+      
+        <div class="box">
+            <img src="laptop.jpeg" alt="" class="maid_img_scroller">
+        </div>
+
+        <div class="box">
+            <img src="laptop.jpeg" alt="" class="maid_img_scroller">
+        </div>
+
+        <div class="box">
+            <img src="laptop.jpeg" alt="" class="maid_img_scroller">
+        </div>
+
+        <div class="box">
+            <img src="laptop.jpeg" alt="" class="maid_img_scroller">
+        </div>
+
+        <div class="box">
+            <img src="laptop.jpeg" alt="" class="maid_img_scroller">
+        </div>
+
+        <div class="box">
+            <img src="laptop.jpeg" alt="" class="maid_img_scroller">
+        </div>
+
+        <div class="box">
+            <img src="laptop.jpeg" alt="" class="maid_img_scroller">
+        </div>
+
+        <div class="box">
+            <img src="laptop.jpeg" alt="" class="maid_img_scroller">
+        </div>
+
+        <div class="box">
+            <img src="laptop.jpeg" alt="" class="maid_img_scroller">
+        </div>
+
+        <div class="box">
+            <img src="laptop.jpeg" alt="" class="maid_img_scroller">
+        </div>
+
+        </div>
+    </div>
+
+</body>
+</html>
