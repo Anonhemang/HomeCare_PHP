45c85bc239f043ddcc54188fd6baa9c7fc719aaa
Anonhemang <hemangthakkar2801@gmail.com>
2022-12-30 10:30:20 +0530

    Contact Page 1.0

---
+++ contact.php
@@ -0,0 +1,93 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="shortcut icon" type="img/jpg" href="img/logo.jpg">
+    <link rel="stylesheet" href="contact.css">
+    <title>Contact Us</title>
+</head>
+<body>
+                                                              <h1>Contact Us</h1>
+    <form>
+        <table cellspacing="20" align="left">
+            
+            <tr>
+                <td>
+                    <input type="text" placeholder="Your Name" required="" autocomplete="off">
+                </td>
+            </tr>
+            
+            <tr>
+                <td>
+                    <input type="email" placeholder="Your Email" required="" autocomplete="off">
+                </td>
+            </tr>
+            
+            <tr>
+                <td>
+                    <input type="tel" placeholder="Your Phone Number" maxlength="10" minlength="10" required="" autocomplete="off">
+                </td>
+            </tr>
+            
+            <tr>
+                <td>
+                    <textarea placeholder="Describe Your Issue...." required="" autocomplete="off"></textarea>
+                </td>
+            </tr>
+            
+            <tr>
+                <td>
+                    <input type="submit" value="Submit" class="submit">
+                </td>
+            </tr>
+        </table>
+    </form>
+
+
+        <form>
+            <table align="center">
+                <tr>
+                    <td>
+                        <a href="tel:+917990391513">  <img src="call.png" alt="Call" title="Call"></a>
+                 
+                    </td>
+                    
+                    <td> 
+                        <a href="tel:+917990391513">+91 7990391513</a>
+
+                    </td>
+                </tr>
+
+                <tr>
+                    <td>
+                        <a href="mailto:210510110099@paruluniversity.ac.in"><img src="mail.png" alt="Mail" title="Mail"></a>
+                    </td>
+
+                    <td>
+                        <a href="mailto:210510110099@paruluniversity.ac.in">hemangthakkar2801@gmail.com</a>
+                    </td>
+                </tr>
+
+                <tr>
+                    <td>
+                        <a href="https://www.google.com/maps/dir/22.3116793,73.1939609/checkmate+security/@22.318182,73.1883726,16z/data=!3m1!4b1!4m9!4m8!1m1!4e1!1m5!1m1!1s0x395fcf36c0000001:0x42409c0231a1128f!2m2!1d73.1901351!2d22.3241986" target="_blank"> 
+                        <img src="location.png" alt="Location" title="Location"></a>
+                    </td>
+
+                    <td>
+                        <a href="https://www.google.com/maps/dir/22.3116793,73.1939609/checkmate+security/@22.318182,73.1883726,16z/data=!3m1!4b1!4m9!4m8!1m1!4e1!1m5!1m1!1s0x395fcf36c0000001:0x42409c0231a1128f!2m2!1d73.1901351!2d22.3241986">GF 6, Amaan Tower,<br> Abbas Tyabji Rd, Suvas Colony,<br> Fatehgunj, Vadodara,<br> Gujarat 390002</a>
+                    </td>
+                </tr>
+                
+                <tr>
+                    <td>
+
+                    </td>
+                </tr>
+            </table>
+        </form>
+
+</body>
+</html>
