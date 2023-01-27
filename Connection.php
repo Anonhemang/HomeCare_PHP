515c201d13029b5bc77b585e9f4f336897983dfe
hemang <hemangthakkar2801@gmail.com>
2023-01-27 12:46:43 +0530

    Connection File

---
+++ Connection.php
@@ -0,0 +1,17 @@
+<?php
+
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "homecare";
+
+$conn = mysqli_connect($servername, $username, $password, $dbname);
+
+if($conn){
+    //echo "connected";
+}
+else{
+    echo "Failed".mysqli_connect_error();
+}
+
+?>
